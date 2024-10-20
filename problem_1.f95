9ef13d1271134dc1d55744acf9278c5cbfd46ecc
Bishnu-GH <bishnuprashadhazarika212f@gmail.com>
2024-10-20 11:55:58 +0530

    Update problem_1.f95

--- problem_1.f95
+++ problem_1.f95
@@ -1,6 +1,6 @@
 program problem1
   implicit none
-  integer :: i = 0
+  integer :: i
   integer :: summation = 0
   do i=0, 999
     if ( mod(i, 3) .eq. 0 .or. mod(i, 5) .eq. 0 ) THEN
@@ -9,8 +9,5 @@ program problem1
     end if
 
   end do
-  print *, "Sum is"
-  print *, summation
+  print *, "Sum is", summation
 end program problem1
-
-program problem2
